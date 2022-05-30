4d147b58bf03d551fb464eb2447f63b34086f6ec
Farhan Ahmed <farhan.sattar@netsoltech.com>
2022-05-30 12:47:04 +0500

    project files

---
+++ src/scheduler/scheduler.controller.ts
@@ -0,0 +1,32 @@
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Scheduler } from './scheduler.entity';
+import { SchedulerService } from './scheduler.service';
+
+@Controller('scheduler')
+export class SchedulerController {
+    constructor(private readonly schedulerService: SchedulerService) { }
+
+    @Post('create')
+    createScheduler(@Body() scheduler: Scheduler): any {
+        this.schedulerService
+            .createScheduler(scheduler)
+            .then((scheduler) => {
+                return scheduler;
+            })
+            .catch((error) => {
+                return { error };
+            });
+    }
+
+    @Get('list')
+    getSchedulers(@Query() dateStart?: string, @Query() dateEnd?: string): any {
+        this.schedulerService
+            .getSchedulers(dateStart, dateEnd)
+            .then((schedulers) => {
+                return schedulers;
+            })
+            .catch((error) => {
+                return { error };
+            });
+    }
+}
